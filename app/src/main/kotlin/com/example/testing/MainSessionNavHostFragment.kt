4b4e803cf6208301f2d71fa0a2c71aa1bea4369e
MSILycanthropy <ethanmichaelk@gmail.com>
2022-02-27 17:26:18 -0600

    init commit

---
+++ app/src/main/kotlin/com/example/testing/MainSessionNavHostFragment.kt
@@ -0,0 +1,31 @@
+package com.example.testing
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import dev.hotwire.turbo.config.TurboPathConfiguration
+import dev.hotwire.turbo.session.TurboSessionNavHostFragment
+import kotlin.reflect.KClass
+
+class MainSessionNavHostFragment : TurboSessionNavHostFragment() {
+    override val sessionName = "main"
+
+    override val startLocation = "https://turbo-native-testing.herokuapp.com/"
+
+    override val registeredActivities: List<KClass<out AppCompatActivity>>
+        get() = listOf(
+            // Leave empty unless you have more
+            // than one TurboActivity in your app
+        )
+
+    override val registeredFragments: List<KClass<out Fragment>>
+        get() = listOf(
+            WebFragment::class
+            // And any other TurboFragments in your app
+        )
+
+    override val pathConfigurationLocation: TurboPathConfiguration.Location
+        get() = TurboPathConfiguration.Location(
+            assetFilePath = "json/configuration.json",
+            remoteFileUrl = "https://turbo.hotwired.dev/demo/configurations/android-v1.json"
+        )
+}
