e2da9abfefc20f7398138a868ebd7db4d259bf7a
MSILycanthropy <ethanmichaelk@gmail.com>
2022-02-27 21:00:42 -0600

    Remove app bar and stuff

--- app/src/main/kotlin/com/example/testing/WebFragment.kt
+++ app/src/main/kotlin/com/example/testing/WebFragment.kt
@@ -1,7 +1,15 @@
 package com.example.testing
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import dev.hotwire.turbo.fragments.TurboWebFragment
 import dev.hotwire.turbo.nav.TurboNavGraphDestination
 
 @TurboNavGraphDestination(uri = "turbo://fragment/web")
-class WebFragment : TurboWebFragment()
+class WebFragment : TurboWebFragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(dev.hotwire.turbo.R.layout.turbo_view, container, false)
+    }
+}
